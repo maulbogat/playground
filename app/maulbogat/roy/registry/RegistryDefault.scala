41bea64d9df68f3134aff3c2b091f2ac0aa11de5
Roy Maulbogat <roym@hibob.com>
2018-05-02 14:28:55 +0300

    refactored tests

--- app/maulbogat/roy/registry/RegistryDefault.scala
+++ app/maulbogat/roy/registry/RegistryDefault.scala
@@ -3,31 +3,31 @@ package maulbogat.roy.registry
 trait RegistryDefault {
   _: NamedRegistry =>
 
-  def getWithDefault(name: String): V
-
-}
-
-trait RegistryDefaultValue extends RegistryDefault {
-  _: NamedRegistry =>
-
-  final override def getWithDefault(name: String): V = getByName(name).getOrElse(default)
-
-  protected def default: V
+  def getWithDefault(name: String): NamedVal
 
 }
 
 trait RegistryDefaultError extends RegistryDefault {
   _: NamedRegistry =>
 
-  final override def getWithDefault(name: String): V = getByName(name).getOrElse(throw error(name))
+  override def getWithDefault(name: String): NamedVal = getByName(name).getOrElse(throw error(name))
 
-  protected def error(name: String): Throwable
+  def error(name: String): Throwable
+
+}
+
+trait RegistryDefaultValue extends RegistryDefault {
+  _: NamedRegistry =>
+
+  override def getWithDefault(name: String): V = getByName(name).getOrElse(default)
+
+  def default: NamedVal
 
 }
 
 trait RegistryDefaultNaive extends RegistryDefault {
   _: NamedRegistry =>
 
-  final override def getWithDefault(name: String): V = getByName(name).get
+  override def getWithDefault(name: String): NamedVal = getByName(name).get
 
 }
